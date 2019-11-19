1d9ee75d33ca6361d7ffee9e0b25b576016ee584
DESKTOP-TE2HGQU\Raziel <henrique.sonserina@hotmail.com>
2019-11-19 19:57:12 -0300

    Criação de model da entidade "questão".

---
+++ src/app/shared/models/question.model.ts
@@ -0,0 +1,16 @@
+export interface Question {
+
+  title: string;
+  img_path: string;
+  support: string;
+  command: string;
+  answer_a: string;
+  answer_b: string;
+  answer_c: string;
+  answer_d: string;
+  answer_e: string;
+  correct_answer: string;
+  author_id: string;
+  dificulty_level: string;
+
+}
