b3433ec0d89f0ad3196819cc3386083cc5636c70
Raziel <henrique.sonserina@hotmail.com>
2019-12-15 03:09:22 -0300

    Implementação de guarda às rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { ValQuestoesComponent } from './val-questoes/val-questoes.component';
 import { BuscaQuestoesComponent } from './busca-questoes/busca-questoes.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'cadastro-questao', component: CadQuestoesComponent},
-  {path: 'validacao-questao', component: ValQuestoesComponent},
-  {path: 'pesquisa-questao', component: BuscaQuestoesComponent}
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginComponent },
+  {path: 'cadastro-questao', component: CadQuestoesComponent, canActivate: [AuthGuard]},
+  {path: 'validacao-questao', component: ValQuestoesComponent, canActivate: [AuthGuard]},
+  {path: 'pesquisa-questao', component: BuscaQuestoesComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
