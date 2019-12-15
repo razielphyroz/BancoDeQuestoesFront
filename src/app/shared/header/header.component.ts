84dca3c5a0f1ea74652523477568027c1fa9d63e
Raziel <henrique.sonserina@hotmail.com>
2019-12-15 03:15:40 -0300

    Atualização de Header.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) { }
+
+  get us() {
+    return this.userService;
+  }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.authService.logout();
+  }
 }
