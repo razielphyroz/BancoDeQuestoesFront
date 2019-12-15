cc8da84b22e04c1300c4a2142bf174c8889335b8
Raziel <henrique.sonserina@hotmail.com>
2019-12-15 03:15:01 -0300

    Definindo ShowHeader através do UserService.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,15 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
 
   title = 'BancoDeQuestoesFront';
-  showHeader = true;
+  showHeader = false;
 
   constructor(
-    private authService: AuthService) {
-  }
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
+    localStorage.clear();
     this.authService.showMenuEmitter.subscribe(
       show => this.showHeader = show
     );
