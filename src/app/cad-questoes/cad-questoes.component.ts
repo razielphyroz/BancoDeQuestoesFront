389bf2b8bad77fbc1fa41143691c4e0318da2650
Raziel <henrique.sonserina@hotmail.com>
2019-12-21 23:56:00 -0300

    Implementação de cadastro de questões.

--- src/app/cad-questoes/cad-questoes.component.ts
+++ src/app/cad-questoes/cad-questoes.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { QuestionService } from '../services/question.service';
+import { Question } from '../core/models/question.model';
+import { UserService } from '../services/user.service';
+import { QuestionStatus } from '../core/enums/question-status';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cad-questoes',
@@ -8,30 +13,65 @@ import { FormBuilder } from '@angular/forms';
 })
 export class CadQuestoesComponent implements OnInit {
 
-  questionForm = this.fb.group({
-    title: [null],
-    img_path: [null],
-    support: [null],
-    command: [null],
-    answer_a: [null],
-    answer_b: [null],
-    answer_c: [null],
-    answer_d: [null],
-    answer_e: [null],
-    correct_answer: [null]
-  });
-
+  questionForm: FormGroup;
+  
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private questionService: QuestionService,
+    private userService: UserService,
+    private toastr: ToastrService
   ) { }
 
   ngOnInit() {
+    this.loadCleanForm();
+  }
+
+  loadCleanForm() {
+    this.questionForm = this.fb.group({
+      title: [null, [Validators.required]],
+      img_path: [null],
+      support: [null, [Validators.required]],
+      command: [null, [Validators.required]],
+      answer_a: [null, [Validators.required]],
+      answer_b: [null, [Validators.required]],
+      answer_c: [null, [Validators.required]],
+      answer_d: [null, [Validators.required]],
+      answer_e: [null, [Validators.required]],
+      correct_answer: [null, [Validators.required]]
+    });
+  }
+
+  get q() {
+    return this.questionForm.value;
   }
 
   submit() {
-    if (this.questionForm.valid) {
-      console.log('Submit(): Form Válido');
-    }
-  }
 
+    if (this.questionForm.invalid) {
+      this.toastr.error("Preencha todos os campos!", null, {positionClass: 'toast-bottom-right'})
+      return
+    }
+
+    const newQuestion: Question = {
+      content: {
+        title: this.q.title,
+        support: this.q.support,
+        command: this.q.command,
+      },
+      answers: {
+        a: this.q.answer_a,
+        b: this.q.answer_b,
+        c: this.q.answer_c,
+        d: this.q.answer_d,
+        e: this.q.answer_e
+      },
+      subjectId: this.userService.getUserSubject(),
+      authorId: this.userService.getUserId(),
+      correctAnswer: this.q.correct_answer,
+      status: QuestionStatus.WAITING_VALIDATION
+    };
+
+    this.questionService.insertNewQuestion(newQuestion);
+    this.loadCleanForm();
+  }
 }
